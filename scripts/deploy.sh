47c9056c4b3350193ddec9530fb70d7397c003df
x19441824 <x19441824@student.ncirl.ie>
2024-03-26 15:00:38 +0000

    Fixing Deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd cd DevOpsSec-CA/
+cd DevOpsSec-CA/
 git pull origin main # Or the branch you want to deploy
 npm install # Install any new dependencies
 pm2 restart all # Restart the application using PM2 or another process manager
