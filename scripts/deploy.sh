750fb93b5e3a2b94741acda954f72877b33775b4
x19441824 <x19441824@student.ncirl.ie>
2024-03-26 14:48:48 +0000

    Adding scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd cd DevOpsSec-CA/
+git pull origin main # Or the branch you want to deploy
+npm install # Install any new dependencies
+pm2 restart all # Restart the application using PM2 or another process manager
