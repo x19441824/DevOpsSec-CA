30ef61ce79f470ed52e4768000fa4a6fb251317c
x19441824 <x19441824@student.ncirl.ie>
2024-03-26 15:09:49 +0000

    Fixing the ec2 AGAIN

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd DevOpsSec-CA/
-git pull origin main # Or the branch you want to deplo
+git pull origin main 
 npm install # Install any new dependencies
 pm2 restart all # Restart the application using PM2 or another process manager
