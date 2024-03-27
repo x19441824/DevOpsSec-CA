e145fadb950274aa828c7cd44c3f0baf0a52a511
x19441824 <x19441824@student.ncirl.ie>
2024-03-27 16:34:47 +0000

    Test

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Simple app" });
+  res.render('index', { title: "Simple CA  app" });
 });
 
 
